5945d7646b616814e1281b0150b6c2f9b8a26199
Saikat-too <singhasaikat37@gmail.com>
2024-04-29 15:07:34 +0600

    check diagnoal to see the player win it or not

--- main.py
+++ main.py
@@ -24,4 +24,8 @@ def has_won(board , player):
         if all(board[row][col] ==player for row in range(3)):
             return True
     
+    #Check Diagonals
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2-i] == player for i in range(3)):
+        return True 
     
+    return False 
