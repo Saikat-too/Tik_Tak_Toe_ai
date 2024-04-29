c7f44122e3d8319c5eb9b2e7a95fc1614bb6e7cb
Saikat-too <singhasaikat37@gmail.com>
2024-04-29 16:00:26 +0600

    fixing but not happening

--- main.py
+++ main.py
@@ -33,14 +33,14 @@ def has_won(board , player):
 
 
 # For evaluating the board state 
-def evaluate_board(board , player , opponent):
+def evaluate_board(board , player , opponent ):
     if has_won(board , player):
         return 1
     elif has_won(board , opponent):
         return -1
     elif is_board_full(board):
         return 0 
-
+    
 # Function to check Possible moves 
 
 def get_possible_moves(board):
@@ -56,14 +56,15 @@ def get_possible_moves(board):
 # Function to implement ucs algorithm to for tic tac toe 
 
 # Function to implement UCS for Tic Tac Toe (iterative)
-def ucs(board, player, opponent):
+def ucs(board, player, opponent , depth):
+    
     # Initialize a queue with the initial board state
-    queue = [(board, 0)]
+    queue = [(board, None, 0)]  # (board, move, score)
     best_score = float('-inf')
     best_move = None
 
     while queue:
-        current_board, current_score = queue.pop(0)
+        current_board, current_move, current_score = queue.pop(0)
 
         # Check if the game is over
         if has_won(current_board, player):
@@ -75,14 +76,17 @@ def ucs(board, player, opponent):
         elif is_board_full(current_board):
             best_score = 0
             break
-
+        elif depth == 4:
+            return best_move , best_score 
+    
+        depth+=1
         # Get all possible moves
         moves = get_possible_moves(current_board)
 
         # Explore each move
         for move in moves:
             new_board = deepcopy(current_board)
-            new_board[move[0]][move[1]] = player
+            new_board[move[0]][move[1]] = opponent  # Make the move for the opponent
 
             # Evaluate the opponent's best response
             opponent_best_score = -ucs(new_board, opponent, player)
@@ -93,15 +97,46 @@ def ucs(board, player, opponent):
                 best_move = move
 
             # Add the new board state to the queue
-            queue.append((new_board, opponent_best_score))
+            queue.append((new_board, move, opponent_best_score))
 
-    # Make the best move on the original board
-    if best_move is not None:
-        board[best_move[0]][best_move[1]] = player
+    return best_move, best_score
 
-    return best_score
-    
+# Main function to play the game
+def play_game():
+    board = [[' ' for _ in range(3)] for _ in range(3)]
+    human_player = 'X'
+    ai_player = 'O'
 
+    while True:
+        print_board(board)
+
+        # Human player's turn
+        move = input("Enter your move (row col): ").split()
+        row, col = int(move[0]), int(move[1])
+
+        if board[row][col] != ' ':
+            print("Invalid move, try again!")
+            continue
+
+        board[row][col] = human_player
+
+        # AI player's turn
+        depth = 0
+        best_move, score = ucs(board, ai_player, human_player , depth)
+        if score == 0:
+            print("It's a tie!")
+            break
+        elif score == -1:
+            print("You win!")
+            break
+        else:
+            if best_move is not None:
+                board[best_move[0]][best_move[1]] = ai_player
+                print("AI's move:")
+            else:
+                print("No move available for AI.")
+
+    print_board(board)
 
 # Function to print the board
 def print_board(board):
@@ -109,40 +144,9 @@ def print_board(board):
         print(' '.join(row))
     print()
     
-# Main function to play the game
-def play_game():
-    board = [[' ' for _ in range(3)] for _ in range(3)]
-    player = 'X'
-    opponent = 'O'
 
-    while True:
-        print_board(board)
-
-        # Human player's turn
-        if player == 'X':
-            move = input("Enter your move (row col): ").split()
-            row, col = int(move[0]), int(move[1])
-
-            if board[row][col] != ' ':
-                print("Invalid move, try again!")
-                continue
-
-            board[row][col] = player
-
-        # AI player's turn
-        else:
-            score = ucs(board, opponent, player)
-            if score == 0:
-                print("It's a tie!")
-                break
-            elif score == -1:
-                print("You win!")
-                break
-            else:
-                print("AI's move:")
-
-        # Switch players
-        player, opponent = opponent, player
+           
+      
 
     print_board(board)
 
