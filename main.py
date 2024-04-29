04cfc00af587c6f15bf5808094e5cfba7596cc94
Saikat-too <singhasaikat37@gmail.com>
2024-04-29 14:57:24 +0600

    Funciton to check if the board is full or not

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 # It is needed to copy the board from move to move
 from copy import deepcopy 
 
+# Utility function to check the if the board is full or not 
+
+def is_board_full(board):
+    for row in board:
+        for cell in row:
+            if cell == ' ':
+                return False
+    
+    return True
+
